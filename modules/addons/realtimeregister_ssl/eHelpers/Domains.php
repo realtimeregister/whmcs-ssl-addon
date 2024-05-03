dd9cd3558be4703739241627119cc1b340938174
Hylke Witjens <hylke.witjens@realtimeregister.com>
2024-05-03 10:26:30 +0200

    RT-14619 finetuned validators for domainnames

--- modules/addons/realtimeregister_ssl/eHelpers/Domains.php
+++ modules/addons/realtimeregister_ssl/eHelpers/Domains.php
@@ -5,8 +5,8 @@ namespace MGModule\RealtimeRegisterSsl\eHelpers;
 class Domains {
     public static function validateDomain($domain)
     {
-        return preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain)
-            && preg_match("/^.{1,253}$/", $domain)
+        return preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])+))+$/i", $domain)
+            && preg_match("/^.{2,253}$/", $domain)
             && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain)
             && preg_match('/[^.\d]/', $domain);
     }
@@ -15,7 +15,7 @@ class Domains {
     {
         //wildcars multilevel domains validation
         return preg_match("/^(\*\.)?([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))+$/i", $domain) //valid chars check
-                && preg_match("/^.{1,253}$/", $domain) //overall length check
+                && preg_match("/^.{2,253}$/", $domain) //overall length check
                 && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain);
     }
 
--- modules/addons/realtimeregister_ssl/eServices/provisioning/GenerateCSR.php
+++ modules/addons/realtimeregister_ssl/eServices/provisioning/GenerateCSR.php
@@ -2,83 +2,78 @@
 
 namespace MGModule\RealtimeRegisterSsl\eServices\provisioning;
 
-use MGModule\RealtimeRegisterSsl as main;
 use Exception;
+use MGModule\RealtimeRegisterSsl\eHelpers\Domains;
+use MGModule\RealtimeRegisterSsl\eRepository\whmcs\service\SSL;
+use MGModule\RealtimeRegisterSsl\mgLibs\Lang;
 
 class GenerateCSR
 {
     private $params;
     private $post;
 
-    function __construct(&$params, &$post)
+    public function __construct(&$params, &$post)
     {
         $this->params = &$params;
-        $this->post   = &$post;
+        $this->post = &$post;
     }
 
     public function run()
     {
-        try
-        {
+        try {
             return $this->GenerateCSR();
-        }
-        catch (\Exception $ex)
-        {
+        } catch (Exception $ex) {
             return json_encode(
-                    [
-                        'success' => 0,
-                        'msg'     => main\mgLibs\Lang::getInstance()->T($ex->getMessage()),
-                    ]
+                [
+                    'success' => 0,
+                    'msg' => Lang::getInstance()->T($ex->getMessage()),
+                ]
             );
         }
     }
 
     private function validateForm()
     {
-        /* if(!preg_match("/^([-a-z0-9]{2,100})\.([a-z\.]{2,8})$/i", $this->post['commonName'])) {
-          throw new Exception('invalidCommonName');
-          } */
-        if (!filter_var($this->post['emailAddress'], FILTER_VALIDATE_EMAIL))
-        {
+        if (!Domains::validateDomain($this->post['commonName'])) {
+            throw new Exception('invalidCommonName');
+        }
+        if (!filter_var($this->post['emailAddress'], FILTER_VALIDATE_EMAIL)) {
             throw new Exception('invalidEmailAddress');
         }
-        if (!preg_match("/^[A-Z]{2}$/i", $this->post['countryName']))
-        {
+        if (!preg_match("/^[A-Z]{2}$/i", $this->post['countryName'])) {
             throw new Exception('invalidCountryCode');
         }
     }
 
     private function GenerateCSR()
     {
-
         $this->validateForm();
 
-        $dn      = [
-            'countryName'            => strtoupper($this->post['countryName']),
-            'stateOrProvinceName'    => $this->post['stateOrProvinceName'],
-            'localityName'           => $this->post['localityName'],
-            'organizationName'       => $this->post['organizationName'],
+        $dn = [
+            'countryName' => strtoupper($this->post['countryName']),
+            'stateOrProvinceName' => $this->post['stateOrProvinceName'],
+            'localityName' => $this->post['localityName'],
+            'organizationName' => $this->post['organizationName'],
             'organizationalUnitName' => $this->post['organizationalUnitName'],
-            'commonName'             => $this->post['commonName'],
-            'emailAddress'           => $this->post['emailAddress'],
+            'commonName' => $this->post['commonName'],
+            'emailAddress' => $this->post['emailAddress'],
         ];
-        
+
         $privKey = openssl_pkey_new([
             "private_key_bits" => 2048,
             "private_key_type" => OPENSSL_KEYTYPE_RSA,
         ]);
-        
+
         if ($privKey) {
             $serviceid = $this->params['serviceid'];
-            if($serviceid == NULL) {
+            if ($serviceid == null) {
                 $serviceid = $this->post['serviceID'];
             }
-            
+
             $saveToDatabase = true;
             if (isset($this->post['doNotSaveToDatabase']) && $this->post['doNotSaveToDatabase']) {
                 $saveToDatabase = false;
             }
-
 
             openssl_pkey_export($privKey, $pKeyOut);
             if ($saveToDatabase) {
@@ -97,24 +92,24 @@ class GenerateCSR
         }
 
         return json_encode(
-                [
-                    'success'     => 1,
-                    'msg'         => main\mgLibs\Lang::getInstance()->T('csrCodeGeneraterdSuccessfully'),
-                    'public_key'  => $csrOut,
-                    'private_key' => encrypt($pKeyOut)
-                ]
+            [
+                'success' => 1,
+                'msg' => Lang::getInstance()->T('csrCodeGeneraterdSuccessfully'),
+                'public_key' => $csrOut,
+                'private_key' => encrypt($pKeyOut)
+            ]
         );
     }
 
     public function savePrivateKeyToDatabase($serviceid, $privKey)
     {
         try {
-            $sslRepo = new \MGModule\RealtimeRegisterSsl\eRepository\whmcs\service\SSL();
+            $sslRepo = new SSL();
 
-            $sslService = $sslRepo->getByServiceId((int) $serviceid);
+            $sslService = $sslRepo->getByServiceId((int)$serviceid);
             $sslService->setConfigdataKey('private_key', encrypt($privKey));
             $sslService->save();
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
             throw new Exception('csrCodeGeneraterFailed');
         }
     }
