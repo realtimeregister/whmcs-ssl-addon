a99053afbe175513067d18e60da28810dd193d89
Hylke Witjens <hylke.witjens@realtimeregister.com>
2024-05-07 14:35:56 +0200

    RT-14619 Resend certificate from the admin

--- modules/addons/realtimeregister_ssl/eServices/provisioning/AdminResendCertificate.php
+++ modules/addons/realtimeregister_ssl/eServices/provisioning/AdminResendCertificate.php
@@ -1,17 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MGModule\RealtimeRegisterSsl\eServices\provisioning;
 
 use Exception;
-use MGModule\RealtimeRegisterSsl\eProviders\ApiProvider;
+use MGModule\RealtimeRegisterSsl\eRepository\whmcs\service\SSL;
 use MGModule\RealtimeRegisterSsl\eServices\EmailTemplateService;
-use SandwaveIo\RealtimeRegister\Api\ProcessesApi;
+use MGModule\RealtimeRegisterSsl\models\apiConfiguration\Repository;
 
 class AdminResendCertificate
 {
     private $p;
-    
-    function __construct(&$params)
+
+    public function __construct(&$params)
     {
         $this->p = &$params;
     }
@@ -25,43 +27,35 @@ class AdminResendCertificate
         }
         return 'success';
     }
-    
+
     private function adminResendCertificate()
     {
-        $ssl = new \MGModule\RealtimeRegisterSsl\eRepository\whmcs\service\SSL();
+        $ssl = new SSL();
         $serviceSSL = $ssl->getByServiceId($this->p['serviceid']);
-        
+
         if (is_null($serviceSSL)) {
             throw new Exception('Create has not been initialized.');
         }
-        
-        if(empty($serviceSSL->remoteid)) {
+
+        if (empty($serviceSSL->remoteid)) {
             throw new Exception('Product not ordered in RealtimeRegisterSsl.');
         }
 
-        /** @var ProcessesApi $processesApi */
-        $processesApi = ApiProvider::getInstance()->getApi(ProcessesApi::class);
-        $orderStatus = $processesApi->get($serviceSSL->remoteid);
-
-        if($orderStatus['status'] !== 'active') {
-            throw new Exception('Can not resend certificate. Order status is different than active.');
-        }
-        
-        if(empty($orderStatus['ca_code'])) {
+        if (empty($serviceSSL->getCa())) {
             throw new Exception('An error occurred. Certificate body is empty.');
         }
-        $apiConf = (new \MGModule\RealtimeRegisterSsl\models\apiConfiguration\Repository())->get();
-        $sendCertyficateTermplate = $apiConf->send_certificate_template;
-        if ($sendCertyficateTermplate == null) {
+        $apiConf = (new Repository())->get();
+        $sendCertificateTemplate = $apiConf->send_certificate_template;
+        if ($sendCertificateTemplate == null) {
             sendMessage(EmailTemplateService::SEND_CERTIFICATE_TEMPLATE_ID, $this->p['serviceid'], [
-                'ssl_certificate' => nl2br($orderStatus['ca_code']),
-                'crt_code' => nl2br($orderStatus['crt_code']),
+                'ssl_certificate' => nl2br($serviceSSL->getCa()),
+                'crt_code' => nl2br($serviceSSL->getCrt()),
             ]);
         } else {
-            $templateName = EmailTemplateService::getTemplateName($sendCertyficateTermplate);
+            $templateName = EmailTemplateService::getTemplateName($sendCertificateTemplate);
             sendMessage($templateName, $this->p['serviceid'], [
-                'ssl_certificate' => nl2br($orderStatus['ca_code']),
-                'crt_code' => nl2br($orderStatus['crt_code']),
+                'ssl_certificate' => nl2br($serviceSSL->getCa()),
+                'crt_code' => nl2br($serviceSSL->getCrt()),
             ]);
         }
     }
