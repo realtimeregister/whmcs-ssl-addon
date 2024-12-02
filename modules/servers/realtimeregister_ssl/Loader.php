f2dfaa538e9de31ae0bac03c53bf660d61b8c428
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2024-12-02 15:24:37 +0100

    RT-15557: formatting

--- modules/servers/realtimeregister_ssl/Loader.php
+++ modules/servers/realtimeregister_ssl/Loader.php
@@ -2,8 +2,8 @@
 
 namespace AddonModule\RealtimeRegisterSsl;
 
-if (!defined('DS')){
-    define('DS',DIRECTORY_SEPARATOR);
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
 }
 
 /**
@@ -12,36 +12,38 @@ if (!defined('DS')){
  * @SuppressWarnings(PHPMD)
  */
 
-if(!class_exists(__NAMESPACE__.'\Loader')){
-    class Loader {
+if (!class_exists(__NAMESPACE__ . '\Loader')) {
+    class Loader
+    {
         static $whmcsDir;
         static $myName = 'realtimeregister_ssl';
         static $availableDirs = [];
 
         /**
          * Set Paths
-         * 
+         *
          * @param string $dir
          */
-        function __construct($dir = null){
-            if(empty($dir)) {
+        function __construct($dir = null)
+        {
+            if (empty($dir)) {
                 $checkDirs = [
-                    'modules'.DS.'addons'.DS,
-                    'modules'.DS.'servers'.DS
+                    'modules' . DS . 'addons' . DS,
+                    'modules' . DS . 'servers' . DS
                 ];
 
-                foreach($checkDirs as $dir) {
+                foreach ($checkDirs as $dir) {
                     if ($pos = strpos(__DIR__, $dir . self::$myName)) {
-                        self::$whmcsDir = substr(__DIR__,0,$pos);
+                        self::$whmcsDir = substr(__DIR__, 0, $pos);
                         break;
                     }
                 }
 
                 if (self::$whmcsDir) {
-                    foreach($checkDirs as $dir) {
-                        $tmp = self::$whmcsDir.$dir.self::$myName;
-                        if(file_exists($tmp)) {
-                            self::$availableDirs[] = $tmp.DS;
+                    foreach ($checkDirs as $dir) {
+                        $tmp = self::$whmcsDir . $dir . self::$myName;
+                        if (file_exists($tmp)) {
+                            self::$availableDirs[] = $tmp . DS;
                         }
                     }
                 }
@@ -49,7 +51,7 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
                 self::$mainDir = $dir;
             }
 
-            spl_autoload_register(array($this,'loader'));
+            spl_autoload_register(array($this, 'loader'));
         }
 
         /**
@@ -60,21 +62,22 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
          * @throws \AddonModule\RealtimeRegisterSsl\addonLibs\exceptions\base
          * @throws \Exception
          */
-        static function loader($className){
+        static function loader($className)
+        {
             if (strpos($className, __NAMESPACE__) === false) {
                 return;
             }
-            
-            $className = substr($className,strlen(__NAMESPACE__));
+
+            $className = substr($className, strlen(__NAMESPACE__));
 
             $originClassName = $className;
             $className = ltrim($className, '\\');
-            $fileName  = '';
+            $fileName = '';
             $namespace = '';
             if ($lastNsPos = strrpos($className, '\\')) {
                 $namespace = substr($className, 0, $lastNsPos);
                 $className = substr($className, $lastNsPos + 1);
-                $fileName  = str_replace('\\', DS, $namespace).DS;
+                $fileName = str_replace('\\', DS, $namespace) . DS;
             }
 
             $fileName .= str_replace('_', DS, $className) . '.php';
@@ -83,8 +86,8 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
 
             $error = [];
 
-            foreach(self::$availableDirs as $dir) {
-                $tmp = $dir.$fileName;
+            foreach (self::$availableDirs as $dir) {
+                $tmp = $dir . $fileName;
 
                 if (file_exists($tmp)) {
                     if ($foundFile) {
@@ -96,18 +99,18 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
             }
 
             if ($foundFile) {
-                require_once $foundFile;            
-                
-                if (!class_exists(__NAMESPACE__.$originClassName)
-                    && !interface_exists(__NAMESPACE__.$originClassName)
-                    && !trait_exists(__NAMESPACE__.$originClassName)) {
-                $error['message'] = 'Unable to find class:'.$originClassName.' in file:'.$foundFile;
-                    $error['code']    = \AddonModule\RealtimeRegisterSsl\addonLibs\exceptions\Codes::MISSING_FILE_CLASS;
+                require_once $foundFile;
+
+                if (!class_exists(__NAMESPACE__ . $originClassName)
+                    && !interface_exists(__NAMESPACE__ . $originClassName)
+                    && !trait_exists(__NAMESPACE__ . $originClassName)) {
+                    $error['message'] = 'Unable to find class:' . $originClassName . ' in file:' . $foundFile;
+                    $error['code'] = \AddonModule\RealtimeRegisterSsl\addonLibs\exceptions\Codes::MISSING_FILE_CLASS;
                 }
-            } 
-            
+            }
+
             if ($error) {
-                if (class_exists(__NAMESPACE__.'\addonLibs\exceptions\Base',false)) {
+                if (class_exists(__NAMESPACE__ . '\addonLibs\exceptions\Base', false)) {
                     throw new \AddonModule\RealtimeRegisterSsl\addonLibs\exceptions\Base($error['message'], $error['code']);
                 } else {
                     throw new \Exception($error['message'], $error['code']);
@@ -116,24 +119,23 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
             return true;
         }
 
-        static function listClassesInNamespace($className){
+        static function listClassesInNamespace($className)
+        {
             $originClassName = $className;
             $className = ltrim($className, '\\');
-            $fileName  = '';
+            $fileName = '';
             $namespace = '';
             if ($lastNsPos = strrpos($className, '\\')) {
                 $namespace = substr($className, 0, $lastNsPos);
                 $className = substr($className, $lastNsPos + 1);
-                $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+                $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
             }
 
             $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className);
 
-            foreach(self::$availableDirs as $dir)
-            {
-                $tmp = $dir.$fileName;
-                if(file_exists($tmp))
-                {
+            foreach (self::$availableDirs as $dir) {
+                $tmp = $dir . $fileName;
+                if (file_exists($tmp)) {
                     $foundFile = $tmp;
                 }
             }
@@ -143,12 +145,12 @@ if(!class_exists(__NAMESPACE__.'\Loader')){
             if ($handle = opendir($foundFile)) {
                 while (false !== ($entry = readdir($handle))) {
                     if (
-                            $entry != "." 
-                            && $entry != ".."
-                            && strpos($entry,'.php') === (strlen($entry)-4)
-                        ) {
+                        $entry != "."
+                        && $entry != ".."
+                        && strpos($entry, '.php') === (strlen($entry) - 4)
+                    ) {
 
-                        $files[] = __NAMESPACE__.'\\'.$originClassName.'\\'.substr($entry, 0,strlen($entry)-4);
+                        $files[] = __NAMESPACE__ . '\\' . $originClassName . '\\' . substr($entry, 0, strlen($entry) - 4);
                     }
                 }
                 closedir($handle);
