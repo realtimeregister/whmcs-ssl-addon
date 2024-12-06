557b31cbb07803f4c4956356ea9f34c3261ddebb
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2024-12-06 09:09:16 +0100

    increment version

--- modules/addons/realtimeregister_ssl/Configuration.php
+++ modules/addons/realtimeregister_ssl/Configuration.php
@@ -58,7 +58,7 @@ class Configuration extends AbstractConfiguration
      * Module version
      * @var string
      */
-    public $version = '0.4.3';
+    public $version = '0.4.4';
 
     private static string $LEGACY_TABLE_PREFIX = 'mgfw_';
 
