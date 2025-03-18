dd1953655872cc58d57e0c38f2ba97c9ccd46bc6
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-03-18 13:42:13 +0100

    .

--- modules/addons/realtimeregister_ssl/tests/eHelpers/ZipFileHelperTest.php
+++ modules/addons/realtimeregister_ssl/tests/eHelpers/ZipFileHelperTest.php
@@ -2,28 +2,28 @@
 
 namespace Tests\eHelpers;
 
-use AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper;
+//use AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper;
 use PHPUnit\Framework\TestCase;
 
 class ZipFileHelperTest extends TestCase
 {
     public function testGetEmptyFile()
     {
-        $zipFileHelper = new ZipFileHelper(file_get_contents(__DIR__ . '/../data/zero-bytes-file.zip'));
+        $zipFileHelper = new \AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper(file_get_contents(__DIR__ . '/../data/zero-bytes-file.zip'));
 
         $this->assertEmpty($zipFileHelper->getFile('something.txt'));
     }
 
     public function testGetFilledFile()
     {
-        $zipFileHelper = new ZipFileHelper(file_get_contents(__DIR__ . '/../data/one-file.zip'));
+        $zipFileHelper = new \AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper(file_get_contents(__DIR__ . '/../data/one-file.zip'));
 
         $this->assertEquals('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', $zipFileHelper->getFile('something.txt'));
     }
 
     public function testGetFilledFileinDirectory()
     {
-        $zipFileHelper = new ZipFileHelper(file_get_contents(__DIR__ . '/../data/file-in-directory.zip'));
+        $zipFileHelper = new \AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper(file_get_contents(__DIR__ . '/../data/file-in-directory.zip'));
 
         $this->assertEquals('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', $zipFileHelper->getFile('test-directory/something.txt'));
 
@@ -34,7 +34,7 @@ class ZipFileHelperTest extends TestCase
 
     public function testFilesInDirectory()
     {
-        $zipFileHelper = new ZipFileHelper(file_get_contents(__DIR__ . '/../data/file-in-directory.zip'));
+        $zipFileHelper = new \AddonModule\RealtimeRegisterSsl\eHelpers\ZipFileHelper(file_get_contents(__DIR__ . '/../data/file-in-directory.zip'));
 
         $this->assertEquals(['test-directory/something.txt'], $zipFileHelper->getNamesOfFilesInDirectory('test-directory'));
 
