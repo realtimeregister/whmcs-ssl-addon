ad68eba97b9b69f8998296f47b94dff7ef4e68b6
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2026-01-27 16:06:29 +0100

    SSL-136: Fix query for retrieving ssl orders

--- modules/addons/realtimeregister_ssl/eRepository/whmcs/service/SSL.php
+++ modules/addons/realtimeregister_ssl/eRepository/whmcs/service/SSL.php
@@ -47,6 +47,7 @@ class SSL
 
     public function getOrdersWithStatus($status) {
         return Model::query()
+            ->select(['tblsslorders.*'])
             ->join('tblhosting', 'tblhosting.id', '=', 'serviceid')
             ->where('tblhosting.domainstatus', 'Active')
             ->whereIn('status', $status)
